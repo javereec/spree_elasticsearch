082ef391d1e61e964cc83e9d47949166b9108ed8
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-05-04 12:39:21 +0900

    Escape search queries

--- lib/spree/search/elasticsearch.rb
+++ lib/spree/search/elasticsearch.rb
@@ -42,11 +42,32 @@ module Spree
         @result.response.aggregations
       end
 
+      module Escaping
+        LUCENE_SPECIAL_CHARACTERS = Regexp.new("(" + %w[
+          + - && || ! ( ) { } [ ] ^ " ~ * ? : \\ /
+        ].map { |s| Regexp.escape(s) }.join("|") + ")")
+
+        LUCENE_BOOLEANS = /\b(AND|OR|NOT)\b/
+
+        def escape(s)
+          # 6 slashes =>
+          #  ruby reads it as 3 backslashes =>
+          #    the first 2 =>
+          #      go into the regex engine which reads it as a single literal backslash
+          #    the last one combined with the "1" to insert the first match group
+          special_chars_escaped = s.gsub(LUCENE_SPECIAL_CHARACTERS, '\\\\\1')
+
+          # Map something like 'fish AND chips' to 'fish "AND" chips', to avoid
+          # Lucene trying to parse it as a query conjunction
+          special_chars_escaped.gsub(LUCENE_BOOLEANS, '"\1"')
+        end
+      end
+
       protected
 
       # converts params to instance variables
       def prepare(params)
-        @query = params[:keywords]
+        @query = Escaping.escape(params[:keywords])
         @sorting = params[:sorting]
         @taxons = params[:taxon] unless params[:taxon].nil?
         @browse_mode = params[:browse_mode] unless params[:browse_mode].nil?
