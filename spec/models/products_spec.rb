921768493be033761426b22d4f97eef9f292c666
Alessandro Lepore <lepore.ale@gmail.com>
2015-10-20 16:50:00 +0200

    try to setup tests (not working)

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 require 'byebug'
 
 module Spree
-  describe Spree::Product do
+  describe Product do
     let(:a_product) { create(:product) }
     let(:another_product) { create(:product) }
 
     before(:each) do
       # for clean testing, delete index, create new one and create/update mapping
-      Spree::Product.delete_all
-      client = Elasticsearch::Client.new log: true, hosts: Spree::ElasticsearchSettings.hosts
-      client.indices.create index: Spree::ElasticsearchSettings.index, body: {}
-      client.indices.put_mapping index: Spree::ElasticsearchSettings.index, type: Spree::Product.type, body: Spree::Product.mapping
+      Product.delete_all
+      client = Elasticsearch::Client.new log: true, hosts: ElasticsearchSettings.hosts
+      # client.indices.create index: ElasticsearchSettings.index, body: {}
+      # client.indices.put_mapping index: ElasticsearchSettings.index, type: Product.document_type, body: Product.mapping
     end
 
     context "#index" do
@@ -27,15 +27,15 @@ module Spree
     context "#elasticsearch_index" do
       it "is set when product is retrieved from the index" do
         product_from_index = Product.get(a_product.id)
-        product_from_index.elasticsearch_index.should == Spree::ElasticsearchSettings.index
+        product_from_index.elasticsearch_index.should == ElasticsearchSettings.index
       end
 
       it "is set when products are searched in the index" do
         a_product.name = "Findme"
         a_product.index
         sleep 3
-        products = Spree::Product.search(query: a_product.name)
-        products.first.elasticsearch_index.should == Spree::ElasticsearchSettings.index
+        products = Product.search(query: a_product.name)
+        products.first.elasticsearch_index.should == ElasticsearchSettings.index
       end
     end
 
@@ -51,7 +51,7 @@ module Spree
         another_product.name = "Foobar"
         another_product.index
         sleep 3 # allow some time for elasticsearch
-        products = Spree::Product.search(query: another_product.name)
+        products = Product.search(query: another_product.name)
         products.total.should == 1
         products.any?{ |p| p.name == another_product.name }.should be_true
       end
@@ -62,7 +62,7 @@ module Spree
         a_product.index
         another_product.index
         sleep 3 # allow some time for elasticsearch
-        products = Spree::Product.search(query: 'Product')
+        products = Product.search(query: 'Product')
         products.total.should == 2
         products.any?{ |p| p.name == a_product.name }.should be_true
         products.any?{ |p| p.name == another_product.name }.should be_true
@@ -74,7 +74,7 @@ module Spree
         another_product.name = "Product 2"
         another_product.index
         sleep 3 # allow some time for elasticsearch
-        products = Spree::Product.search
+        products = Product.search
         products.total.should == 2
         products.to_a[0].name.should == a_product.name
         products.to_a[1].name.should == another_product.name
@@ -86,7 +86,7 @@ module Spree
         another_product.price = 3
         another_product.index
         sleep 3 # allow some time for elasticsearch
-        products = Spree::Product.search(price_min: 2, price_max: 4)
+        products = Product.search(price_min: 2, price_max: 4)
         products.total.should == 1
         products.to_a[0].name.should == another_product.name
       end
@@ -97,7 +97,7 @@ module Spree
         another_product.price = 3
         another_product.index
         sleep 3 # allow some time for elasticsearch
-        products = Spree::Product.search(price_min: 4, price_max: 2)
+        products = Product.search(price_min: 4, price_max: 2)
         products.total.should == 2
       end
 
@@ -107,57 +107,57 @@ module Spree
         another_product.price = 3
         another_product.index
         sleep 3 # allow some time for elasticsearch
-        products = Spree::Product.search(price_min: 2)
+        products = Product.search(price_min: 2)
         products.total.should == 2
-        products = Spree::Product.search(price_max: 2)
+        products = Product.search(price_max: 2)
         products.total.should == 2
-        products = Spree::Product.search(price_min: nil, price_max: nil)
+        products = Product.search(price_min: nil, price_max: nil)
         products.total.should == 2
       end
 
       context 'properties' do
         it "allows searching on property" do
           a_product.set_property('the_prop', 'a_value')
-          product = Spree::Product.find(a_product.id)
+          product = Product.find(a_product.id)
           product.save
           sleep 3 # allow some time for elasticsearch
-          products = Spree::Product.search(properties: { 'the_prop' => ['a_value'] })
+          products = Product.search(properties: { 'the_prop' => ['a_value'] })
           products.count.should == 1
           products.to_a[0].name.should == product.name
         end
 
         it "allows searching on different property values (OR relation)" do
           a_product.set_property('the_prop', 'a_value')
-          product_one = Spree::Product.find(a_product.id)
+          product_one = Product.find(a_product.id)
           product_one.save
           another_product.set_property('the_prop', 'b_value')
-          product_two = Spree::Product.find(another_product.id)
+          product_two = Product.find(another_product.id)
           product_two.save
           sleep 3 # allow some time for elasticsearch
-          products = Spree::Product.search(properties: { 'the_prop' => ['a_value', 'b_value'] })
+          products = Product.search(properties: { 'the_prop' => ['a_value', 'b_value'] })
           products.count.should == 2
           products.to_a.find {|p| p.name == product_one.name}.should_not be_nil
           products.to_a.find {|p| p.name == product_two.name}.should_not be_nil
-        end 
+        end
 
         it "allows searching on different properties (AND relation)" do
           a_product.set_property('the_prop', 'a_value')
           a_product.set_property('another_prop', 'a_value')
-          product = Spree::Product.find(a_product.id)
+          product = Product.find(a_product.id)
           product.save
           another_product.set_property('the_prop', 'a_value')
           another_product.set_property('another_prop', 'b_value')
-          Spree::Product.find(another_product.id).save
+          Product.find(another_product.id).save
           sleep 3 # allow some time for elasticsearch
-          products = Spree::Product.search(properties: { 'the_prop' => ['a_value'], 'another_prop' => ['a_value'] })
+          products = Product.search(properties: { 'the_prop' => ['a_value'], 'another_prop' => ['a_value'] })
           products.count.should == 1
           products.to_a[0].name.should == product.name
-        end        
+        end
       end
 
       context 'facets' do
         it "contains price facet" do
-          products = Spree::Product.search(name: a_product.name)
+          products = Product.search(name: a_product.name)
           facet = products.facets.find {|facet| facet.name == "price"}
           facet.should_not be_nil
           facet.type.should == "statistical"
@@ -189,9 +189,9 @@ module Spree
       end
     end
 
-    context 'type' do
+    context 'document_type' do
       it 'returns the name of the class' do
-        Product.type.should == 'spree_product'
+        Product.document_type.should == 'spree_product'
       end
     end
 
@@ -199,12 +199,12 @@ module Spree
       it 'returns maximum updated_at for caching purposes' do
         a_product.index
         sleep 2
-        products = Spree::Product.search(query: "name:\"#{a_product.name}\"")
+        products = Product.search(query: "name:\"#{a_product.name}\"")
         products.maximum(:updated_at).to_i.should == a_product.updated_at.to_i # == doesn't seem to work for ActiveSupport::TimeWithZone, converting to integer
       end
 
       it 'returns 0 as maximum updated_at when no results' do
-        products = Spree::Product.search(query: 'qwertyasdfg')
+        products = Product.search(query: 'qwertyasdfg')
         products.maximum(:updated_at).should == 0
       end
     end
